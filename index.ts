59b9dc67ed5238193145a9f21ad633cd850621fb
elitemodzpr <elitemodzpr@gmail.com>
2022-03-07 20:27:42 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
-
+<p>Escuchas las mejores canciones cristianas nuevas y viejas para la alabanza de nuestro Dios</p>
+<div class="powr-music-player" id="35080145_1644368468"></div><script src="https://www.powr.io/powr.js?platform=html"></script>
+     </div>
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
